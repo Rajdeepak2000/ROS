30867ccd4a13352ccab7a6a82ea18c92b69713dc
Rajdeepak2000 <55258843+Rajdeepak2000@users.noreply.github.com>
2020-11-05 23:38:16 +0530

    Update gps.py

--- Quadcopter/gps.py
+++ Quadcopter/gps.py
@@ -34,9 +34,19 @@ class Position():
         self.lat=0.0
         self.long=0.0
         self.alt=0.0
+
+
+
         self.command=rospy.Publisher('/drone_command',edrone_cmd,queue_size=1)
         rospy.Subscriber('/edrone/gps',NavSatFix,self.gps_data)
         rospy.Subscriber('/input_gps',custom_gps,self.input_gps_callback)
+
+        #addtional
+        self.pub_lat_error=rospy.Publisher('/n_lat_error',Float64,queue_size=1)
+        self.pub_long_error=rospy.Publisher('/n_long_error',Float64,queue_size=1)
+        self.pub_alt_error=rospy.Publisher('/n_alt_error',Float64,queue_size=1)
+        self.pub_out_pitch=rospy.Publisher('/n_out_pitch',Float64,queue_size=1)
+        self.pub_out_yaw=rospy.Publisher('/n_out_yaw',Float64,queue_size=1)
 
 
 
@@ -65,9 +75,16 @@ class Position():
 
         self.out_pitch=self.lat_error*50.0
         self.out_yaw=self.long_error*50.0
-        self.command.rcPitch=self.out_pitch
-        self.command.rcYaw=self.out_yaw
-        self.command.publish(edrone_cmd)
+        self.nav_cmd.rcPitch=self.out_pitch
+        self.nav_cmd.rcYaw=self.out_yaw
+        self.command.publish(self.nav_cmd)
+
+        #additional
+        self.pub_lat_error.publish(self.lat_error)
+        self.pub_long_error.publish(self.long_error)
+        self.pub_alt_error.publish(self.alt_error)
+        self.pub_out_pitch.publish(self.nav_cmd.rcPitch)
+        self.pub_out_yaw.publish(self.nav_cmd.rcYaw)
 
 if __name__== '__main__':
           pose=Position()
